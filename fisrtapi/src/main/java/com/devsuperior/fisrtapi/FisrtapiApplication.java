c37785df5db0ef321ca038725023f950a2a0bfdf
leandro518 <leandro518@gmail.com>
2021-02-21 19:49:42 -0300

    exemplo API get

---
+++ fisrtapi/src/main/java/com/devsuperior/fisrtapi/FisrtapiApplication.java
@@ -0,0 +1,54 @@
+package com.devsuperior.fisrtapi;
+
+import com.devsuperior.entities.Category;
+import com.devsuperior.entities.Product;
+import com.devsuperior.repositories.CategoryRepository;
+import com.devsuperior.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+@ComponentScan({"com.devsuperior"})
+@EntityScan("com.devsuperior.entities")
+@EnableJpaRepositories("com.devsuperior.repositories")
+public class FisrtapiApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(FisrtapiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+
+		Product p1 = new Product(null, "TV", 2200.00, cat1);
+		Product p2 = new Product(null, "Domain Driven Design", 120.00, cat2);
+		Product p3 = new Product(null, "PS5", 2800.00, cat1);
+		Product p4 = new Product(null, "Docker", 100.00, cat2);
+
+		cat1.getProducts().addAll(Arrays.asList(p1, p3));
+		cat2.getProducts().addAll(Arrays.asList(p2, p4));
+
+		categoryRepository.save(cat1);
+		categoryRepository.save(cat2);
+
+		productRepository.save(p1);
+		productRepository.save(p2);
+		productRepository.save(p3);
+		productRepository.save(p4);
+	}
+}
---
+++ fisrtapi/src/test/java/com/devsuperior/fisrtapi/FisrtapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.fisrtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisrtapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
